c66eda1072f8d62eb07aa8f6502d95fd2c640892
kajaviousmontoya <montoyakajavious@gmail.com>
2026-04-07 08:04:37 -0700

    Updated with 2 more print statements

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -2,8 +2,9 @@ package com.pluralsight;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("A nice little message");
-        System.out.println("One more");
+        System.out.println("One more commit!");
+        System.out.println("One more push!");
+
 
 
     }
